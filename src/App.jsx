147c36c2de15f142204c70d3bbdab24edc2da42f
Breyner <usstariiz@gmail.com>
2023-03-03 15:02:18 -0500

    añadiendo el modal del formulario al dar click en el boton de nuevo gasto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Modal from './components/Modal'
 import iconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 const [presupuesto, setPresupuesto] = useState(0)
 const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+const [modal, setModal] = useState(false)
+const [animarModal, setAnimarModal] = useState(false)
+
+const handleNuevoGasto = ()=>{
+   setModal(true)
+
+   setTimeout(() => {
+      setAnimarModal(true)
+   }, 400);
+}
   return (
     <div>
       <Header
@@ -17,8 +28,18 @@ const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
       {isValidPresupuesto && (
 
          <div className="nuevo-gasto">
-            <img src={iconoNuevoGasto} alt="nuevo gasto" />
+            <img src={iconoNuevoGasto} alt="nuevo gasto"
+               onClick={handleNuevoGasto}
+            />
          </div>
+      )}
+
+      {modal && (
+         <Modal 
+         setModal={setModal}
+         animarModal={animarModal}
+         setAnimarModal={setAnimarModal}
+         />
       )}
     </div>
   )
